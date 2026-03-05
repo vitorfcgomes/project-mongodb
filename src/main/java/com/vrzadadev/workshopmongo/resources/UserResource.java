c102db5d49f3183b3bda09c78737a10b65d765a1
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-03-05 14:01:09 -0300

    Added Post endpoint

--- src/main/java/com/vrzadadev/workshopmongo/resources/UserResource.java
+++ src/main/java/com/vrzadadev/workshopmongo/resources/UserResource.java
@@ -13,51 +13,62 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.vrzadadev.workshopmongo.domain.Post;
 import com.vrzadadev.workshopmongo.domain.User;
 import com.vrzadadev.workshopmongo.dto.UserDTO;
 import com.vrzadadev.workshopmongo.services.UserService;
 
 @RestController // anotation para falar que é um recurso rest
-@RequestMapping(value ="/users") // caminho do endpoint
+@RequestMapping(value = "/users") // caminho do endpoint
 public class UserResource {
 
 	@Autowired // controlador REST acessa o serviço
 	private UserService service;
 
-	@RequestMapping(method=RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<UserDTO>> findAll() {
 
 		List<User> list = service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto); // ok = metodo que vai instanciar o RE já com o código de resposta
-												// HTTP(que a resposta ocorreu com sucesso)
-												// body = para definir o corpo da resposta, aqui sendo a lista que
-												// montei acima
+													// HTTP(que a resposta ocorreu com sucesso)
+													// body = para definir o corpo da resposta, aqui sendo a lista que
+													// montei acima
 	}
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(new UserDTO(obj));
 	}
-	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
 		User obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
-		return ResponseEntity.created(uri).build(); // comando que retorna uma resposta vazia com o código 201 e com o cabeçalho contendo o novo recurso criado
-		
+		return ResponseEntity.created(uri).build(); // comando que retorna uma resposta vazia com o código 201 e com o
+													// cabeçalho contendo o novo recurso criado
+
 	}
-	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.Delete(id);
 		return ResponseEntity.noContent().build();
 	}
-	@RequestMapping(value="/{id}",method=RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
 		User obj = service.fromDTO(objDto);
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
-		
+
+	}
+	@RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
