5a232c1064a967de78183e3792faf38971fa67f7
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-03-05 13:46:19 -0300

    Added UserDTO projection

--- src/main/java/com/vrzadadev/workshopmongo/config/Instantation.java
+++ src/main/java/com/vrzadadev/workshopmongo/config/Instantation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.vrzadadev.workshopmongo.domain.Post;
 import com.vrzadadev.workshopmongo.domain.User;
+import com.vrzadadev.workshopmongo.dto.AuthorDTO;
 import com.vrzadadev.workshopmongo.repository.PostRepository;
 import com.vrzadadev.workshopmongo.repository.UserRepository;
 
@@ -33,11 +34,10 @@ public class Instantation implements CommandLineRunner {
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good Morning", "I woke up happy today", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good Morning", "I woke up happy today",new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
