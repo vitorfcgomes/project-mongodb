480d86ecc17782562ff1dbfe43e3a9244361eec8
Vitor Ferreira <vrferreiragomes@gmail.com>
2026-03-06 14:36:22 -0300

    Added comments by posts

--- src/main/java/com/vrzadadev/workshopmongo/config/Instantation.java
+++ src/main/java/com/vrzadadev/workshopmongo/config/Instantation.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
 import com.vrzadadev.workshopmongo.domain.Post;
 import com.vrzadadev.workshopmongo.domain.User;
 import com.vrzadadev.workshopmongo.dto.AuthorDTO;
+import com.vrzadadev.workshopmongo.dto.CommentDTO;
 import com.vrzadadev.workshopmongo.repository.PostRepository;
 import com.vrzadadev.workshopmongo.repository.UserRepository;
 
@@ -38,6 +39,14 @@ public class Instantation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para São Paulo Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Good Morning", "I woke up happy today",new AuthorDTO(maria));
+		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano! ", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite ", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia! ", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
